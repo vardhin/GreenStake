df91eb2fde003356afc409c6876e03a2fe0206e0
vardhin <gamidisurya.vardhin2022@vitstudent.ac.in>
2025-02-07 02:55:17 +0530

    new

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'react-native';
-import { AuthProvider } from '@/contexts/AuthContext';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
