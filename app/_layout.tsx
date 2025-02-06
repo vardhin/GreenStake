9ab0199682b881c788ab8f98a53d24a74fc6039b
vardhin <gamidisurya.vardhin2022@vitstudent.ac.in>
2025-02-07 00:34:12 +0530

    ok

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <>
-      <Stack />
-      <StatusBar style="auto" />
-    </>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
   );
 }
