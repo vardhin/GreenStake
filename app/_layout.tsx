021d2d8fb99238d855205a3b4545322c89af53f7
vardhin <gamidisurya.vardhin2022@vitstudent.ac.in>
2025-02-07 03:09:21 +0530

    new

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,11 +6,11 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    
+    <>
       <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
-    
+    </>
   );
 }
